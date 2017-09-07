a10b19b316abc4edb3b0a39684a1a7b37d174736
Michał Januszkiewicz <michal.januszkiewicz@monterail.com>
2017-09-07 13:49:15 +0200

    Add basic team logic

--- app.rb
+++ app.rb
@@ -1,16 +1,23 @@
 require 'sinatra'
+require 'pry'
 require_relative 'app/slack_authorizer'
+require_relative 'app/team'
 
 use SlackAuthorizer
 
+team = Team.new
+
 post '/slack/foosball' do
-  "Match opened"
+  team.create(params["user_name"])
+  "@here Let's play a game.\nTeam status: #{team.members_list}"
 end
 
 post '/slack/+' do
-  "Joined the team"
+  notification = team.add_member(params["user_name"])
+  "Team status: #{team.members_list}\n#{notification}"
 end
 
 post '/slack/-' do
-  "Left the team"
+  team.delete_member(params["user_name"])
+  "Team status: #{team.members_list}"
 end
---
+++ app/team.rb
@@ -0,0 +1,32 @@
+class Team
+  attr_accessor :members
+
+  def initialize()
+    @members = []
+  end
+
+  def create(initiator)
+    @members = [initiator]
+  end
+
+  def add_member(member)
+    # return if members.include? member
+    @members << member
+    notify_members if members.size == 4
+  end
+
+  def delete_member(member)
+    @members.delete(member)
+    @members
+  end
+
+  def members_list
+    @members.join(", ")
+  end
+
+  private
+
+  def notify_members
+    @members.map { |member| "@" + member }.join(", ")
+  end
+end
